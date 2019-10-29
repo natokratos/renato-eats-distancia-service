c037bbb80cab7159d9cdb3fc6b8a82293a6329e1
natokratos <micro8696@caelum114-04>
2019-10-29 09:36:11 -0300

    Versao Inicial

---
+++ src/test/java/br/com/caelum/eatsdistanciaservice/EatsDistanciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.eatsdistanciaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsDistanciaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
